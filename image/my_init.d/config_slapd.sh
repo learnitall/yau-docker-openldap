f8d9e319e479677de624aed325b5d931b89b092f
Ryan Drew <ryan@thedrews.org>
2019-01-08 16:54:09 -0700

    Rename confldap to config_slapd, accounts for permission issues while running slapd as non-root user

---
+++ image/my_init.d/config_slapd.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -x -e
+# Set proper permissions on slapd config
+chown -v ldap:ldap "${SLAPD_CONFIG_LDIF}"
+chmod -v 640 "${SLAPD_CONFIG_LDIF}"
+
+# Set proper permissions on all directories used for databases
+grep "olcDbDirectory:" "${SLAPD_CONFIG_LDIF}" | awk -F ":" '{print $2}' | while IFS= read -r db; do
+    if [ -n "$db" ]; then
+        mkdir -p $db
+        chown -vR ldap:ldap $db
+        chmod -vR 777 $db
+    fi 
+done
+
+
+# Remove existing configuration, if needed, then create empty slapd.d
+if [ -d "/usr/local/etc/openldap/slapd.d" ]; then
+    rm -rfv /usr/local/etc/openldap/slapd.d
+fi
+mkdir /usr/local/etc/openldap/slapd.d
+chown -v ldap:ldap /usr/local/etc/openldap/slapd.d
+
+
+# Create slapd configuration from the slapd config ldif
+/sbin/setuser ldap /usr/local/sbin/slapadd -n0 -v -F /usr/local/etc/openldap/slapd.d -l "${SLAPD_CONFIG_LDIF}"
+
+# Set proper permissions from slapd.pid and slapd.args files if needed, as since we are running 
+# slapd as a non-root user they need to be created beforehand
+# Look for pid and args file definitions in the given slapd ldif config, setting defaults if not given
+pidFileLdif="$(grep "olcPidFile:" "${SLAPD_CONFIG_LDIF}" | awk -F ":" '{print $2}')"
+pidFile=${pidFileLdif:="/var/run/slapd.pid"}
+argsFileLdif="$(grep "olcArgsFile:" "${SLAPD_CONFIG_LDIF}" | awk -F ":" '{print $2}')"
+argsFile=${argsFileLdif:="/var/slapd.args"}
+
+# Create the pid file and all parent directories
+for f in $pidFile $argsFile; do
+    if [ ! -e "$f" ]; then
+        mkdir -p $f 
+        rm -r $f
+        touch $f
+    fi
+done
+
+# Give ownership of the pid file to ldap:ldap, permissions set at 640
+chown -v ldap:ldap $pidFile $argsFile
+chmod -v 640 $pidFile $argsFile
+
+# Test configuration
+/sbin/setuser ldap /usr/local/sbin/slaptest -n0 -v -F /usr/local/etc/openldap/slapd.d
--- image/my_init.d/confldap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/etc/openldap/openldap-docker/load_secrets.py $OPENLDAP_CONF_FILE --secrets-dir $OPENLDAP_SECRETS_DIR | ldapmodify -Y EXTERNAL -h ldapi:///
